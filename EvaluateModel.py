5ca0429f0f8eea21fff0e7305f0d5cd71c4b6d40
Arif Rosyid Prasetyio <aprasetyio@gmail.com>
2023-11-26 11:41:27 +0700

    update

--- EvaluateModel.py
+++ EvaluateModel.py
@@ -11,7 +11,7 @@ trainer = Trainer(cfg, mode='eval')
 # Nonaktifkan normalisasi fusi dan mode penyebaran, dan muat bobot model
 trainer.model.fuse_norm = False
 trainer.model.deploy = False
-trainer.load_weights('model/picodet_s_320_coco_lcnet_udtiri_lama/best_model_epoch_209.pdparams')
+trainer.load_weights('model/picodet_s_320_coco_lcnet_udtiri_lamaBaru/best_model_epoch_149.pdparams')
 
 # Atur model ke mode evaluasi
 trainer.evaluate()
